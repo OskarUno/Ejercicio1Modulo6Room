d5f08f163e1426f2a58c032b72b15557bfc755a7
OskarUno <oorellana80@gmail.com>
2023-07-27 22:26:26 -0400

    Ejercicio 3. Se agrega ViewModel y se le pasa el repositorio se agrega dependencia lifeCycle se agrega funcion obtenerTareas e insertar tareas en repositorio

--- app/src/main/java/com/awakelab/oskar/ejercicio1modulo6room/TareaViewModel.kt
+++ app/src/main/java/com/awakelab/oskar/ejercicio1modulo6room/TareaViewModel.kt
@@ -6,19 +6,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class TareaViewModel (aplicacion : Application) : AndroidViewModel(aplicacion){
-    private val repositorio:Repositorio
+class TareaViewModel(aplicacion: Application) : AndroidViewModel(aplicacion) {
+    private val repositorio: Repositorio
 
-    init{
+    init {
         repositorio = Repositorio(TareaBaseDatos.getDatabase(aplicacion).getTaskDao())
-
     }
+
     fun obtenerTareas(): LiveData<List<Tarea>> {
         return repositorio.getTareas()
     }
 
     //viewModelScope.launch El view model crea una corrutina para ejecutar las funciones suspendidas
-    fun insertarTarea(tarea: Tarea) = viewModelScope.launch{
+    fun insertarTarea(tarea: Tarea) = viewModelScope.launch {
         repositorio.insertTask(tarea)
     }
 
